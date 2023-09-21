9ec951c5581a1f3e32d4d7e9a04d7f8f17a7ede0
hinaryy <shpak1352@gmail.com>
2023-09-21 11:16:51 +0300

    edit modal window added

--- static/src/script.js
+++ static/src/script.js
@@ -1,44 +1,44 @@
 function init() {
-  console.log("init")
-  let getBtn = document.getElementById("getBtn")
-  let postBtn = document.getElementById("postBtn")
-  let clearBtn = document.getElementById("clearBtn")
-  window.onload = getWords()
+  console.log("init");
+  let getBtn = document.getElementById("getBtn");
+  let postBtn = document.getElementById("postBtn");
+  let clearBtn = document.getElementById("clearBtn");
+  window.onload = getWords();
 
   getBtn.onclick = () => {
-    console.log("getBtn click")
-    getWords()
-  }
+    console.log("getBtn click");
+    getWords();
+  };
 
   postBtn.onclick = () => {
-    console.log("postBtn click")
-    postWord()
-  }
+    console.log("postBtn click");
+    postWord();
+  };
 
   clearBtn.onclick = () => {
-    console.log("clearBtn click")
-    clearWords()
-  }
+    console.log("clearBtn click");
+    clearWords();
+  };
 }
 
 function WordUI(word) {
-  const div = document.createElement("div")
+  const div = document.createElement("div");
 
   for (const key in word) {
     if (key == "id") {
-      continue
+      continue;
     }
-    const f = field(key, word[key])
-    div.classList.add("word-card")
-    div.appendChild(f)
+    const f = field(key, word[key]);
+    div.classList.add("word-card");
+    div.appendChild(f);
   }
-  return div
+  return div;
 }
 
 function field(key, value) {
-  const div = document.createElement("div")
-  div.innerHTML = `${key}: ${value}`
-  return div
+  const div = document.createElement("div");
+  div.innerHTML = `${key}: ${value}`;
+  return div;
 }
 
 function getWords() {
@@ -46,59 +46,86 @@ function getWords() {
     method: "GET",
   })
     .then(async (res) => {
-      const words = await res.json()
-      result.innerHTML = ""
-      var nextId = 1
+      const words = await res.json();
+      result.innerHTML = "";
       if (words != null) {
         words.forEach((w) => {
-          const wordUI = WordUI(w)
+          const wordUI = WordUI(w);
 
-          let wordId = "word-" + w.id
-          console.log(wordId)
+          let wordId = "word-" + w.id;
+          let id = wordId.split("-")[1];
 
-          wordUI.id = wordId
-          wordUI.style.position = "relative"
+          wordUI.id = wordId;
+          wordUI.style.position = "relative";
 
-          let deleteBtn = document.createElement("button")
-          deleteBtn.textContent = "x"
-          deleteBtn.classList.add("deleteBtn")
+          let deleteBtn = document.createElement("button");
+          deleteBtn.textContent = "x";
+          deleteBtn.classList.add("deleteBtn");
+
           deleteBtn.onclick = () => {
-            console.log("deleteBtn click")
-            let id = wordId.split("-")[1]
-            deleteWord(id)
-            let divToRemove = document.getElementById(wordId)
+            console.log("deleteBtn click");
+            deleteWord(id);
+            let divToRemove = document.getElementById(wordId);
             if (divToRemove) {
-              divToRemove.parentNode.removeChild(divToRemove)
-              console.log(wordId)
+              divToRemove.parentNode.removeChild(divToRemove);
+              console.log(wordId);
             }
-          }
+          };
 
-          wordUI.appendChild(deleteBtn)
+          let editBtn = document.createElement("button");
+          editBtn.textContent = "/";
+          editBtn.classList.add("editBtn");
 
-          result.appendChild(wordUI)
-          nextId++
-        })
+          editBtn.onclick = () => {
+            console.log("editBtn click");
+            let modal = document.getElementById("editModal");
+            modal.style.display = "flex";
+
+            let editedRuInput = document.getElementById("editedRu");
+            let editedEnInput = document.getElementById("editedEn");
+
+            editedRuInput.value = w.Russian;
+            editedEnInput.value = w.English;
+
+            let confirmBtn = document.getElementById("confirmBtn");
+            let discardBtn = document.getElementById("discardBtn");
+
+            confirmBtn.onclick = () => {
+              editWord(id, editedRuInput.value, editedEnInput.value);
+              modal.style.display = "none";
+            };
+
+            discardBtn.onclick = () => {
+              modal.style.display = "none";
+            };
+          };
+
+          wordUI.appendChild(deleteBtn);
+          wordUI.appendChild(editBtn);
+
+          result.appendChild(wordUI);
+        });
       }
     })
     .catch((error) => {
-      console.error("getWords error:", error)
-    })
+      console.error("getWords error:", error);
+    });
 }
 
 function postWord() {
-  let ru_word = document.getElementById("ru-word")
-  let en_word = document.getElementById("en-word")
+  let ru_word = document.getElementById("ru-word");
+  let en_word = document.getElementById("en-word");
 
   let data = {
     Russian: ru_word.value,
     English: en_word.value,
-  }
+  };
   if (ru_word.value == "" || en_word.value == "") {
-    throw "Words can not be empty"
+    throw "Words can not be empty";
   }
 
-  ru_word.value = ""
-  en_word.value = ""
+  ru_word.value = "";
+  en_word.value = "";
 
   fetch("http://localhost:8080/words", {
     method: "POST",
@@ -109,12 +136,28 @@ function postWord() {
   })
     .then(async (res) => res.text())
     .then(async (data) => {
-      getWords()
+      getWords();
     })
     .catch((error) => {
-      console.error("postWord error:", error)
-    })
+      console.error("postWord error:", error);
+    });
+}
 
+function editWord(id, ru, en) {
+  fetch("http://localhost:8080/words/" + id, {
+    method: "PUT",
+    body: JSON.stringify(id),
+  })
+    .then(async (res) => {
+      res.text();
+
+      console.log("Ru:", ru);
+      console.log("En:", en);
+      getWords();
+    })
+    .catch((error) => {
+      console.log("editWord error:", error);
+    });
 }
 
 function clearWords() {
@@ -122,22 +165,22 @@ function clearWords() {
     method: "DELETE",
   })
     .then(() => {
-      getWords()
+      getWords();
     })
     .catch((error) => {
-      console.error("clearWords error: ", error)
-    })
+      console.error("clearWords error: ", error);
+    });
 }
 
 function deleteWord(id) {
   fetch("http://localhost:8080/words/" + id, {
     method: "DELETE",
-    body: JSON.stringify(id)
+    body: JSON.stringify(id),
   })
     .then(async (res) => {
-      res.text()
+      res.text();
     })
     .catch((error) => {
-      console.log("deleteWord error:", error)
-    })
+      console.log("deleteWord error:", error);
+    });
 }
